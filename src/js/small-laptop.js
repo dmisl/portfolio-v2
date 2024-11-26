cb3f61da181cd3a1e87e975a39b4179020b57bfa
unknown <rithlesscorp@gmail.com>
2024-11-26 22:52:56 +0100

    small laptops fix

--- src/js/small-laptop.js
+++ src/js/small-laptop.js
@@ -1,9 +1,13 @@
-if(window.innerWidth <= 1250)
+function check()
 {
-    document.querySelector('.about-me .text').innerHTML = `Hello, i’m <span>dmisl</span>!<br><br>I’m a self-taught full-stack developer based in <br><span>Warsaw, Poland</span>. I can develop responsive websites<br>from scratch and raise them into modern user-friendly<br>web experiences.<br><br>Transforming my creativity and knowledge into a websites has been my passion for years. I have been helping various clients to establish <br>their presence online. I always strive to learn about the newest technologies and frameworks.`
-}
-window.addEventListener('resize', () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    console.log(`New resolution: ${width}x${height}`);
-});
+    if(window.innerWidth <= 1250)
+    {
+        document.querySelector('.about-me .text').innerHTML = `Hello, i’m <span>dmisl</span>!<br><br>I’m a self-taught full-stack developer based in <br><span>Warsaw, Poland</span>. I can develop responsive websites<br>from scratch and raise them into modern user-friendly<br>web experiences.<br><br>Transforming my creativity and knowledge into a websites has been my passion for years. I have been helping various clients to establish <br>their presence online. I always strive to learn about the newest technologies and frameworks.`
+    } else
+    {
+        document.querySelector('.about-me .text').innerHTML = `Hello, i’m <span>dmisl</span>!<br><br>I’m a self-taught full-stack developer based in <span>Warsaw, Poland</span>. I can develop responsive websites from<br>scratch and raise them into modern user-friendly web experiences.<br><br>Transforming my creativity and knowledge into a websites has been my passion for years. I have been helping various clients to establish <br>their presence online. I always strive to learn about the newest technologies and frameworks.`
+    }
+}
+window.addEventListener('resize', check);
