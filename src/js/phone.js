f526da37c00ecc403bd43f777e91b8aba6c3bbcf
unknown <rithlesscorp@gmail.com>
2024-11-30 17:28:35 +0100

    hopefully working container

--- src/js/phone.js
+++ src/js/phone.js
@@ -16,6 +16,23 @@ if(window.innerWidth <= 500)
 }
 function phone_check()
 {
-  console.log('change something on mobile change')
+  const element = document.querySelector('body');
+    
+  const rect = element.getBoundingClientRect();
+  
+  let maxWidth = rect.width+'px'
+  console.log(maxWidth)
+  console.log(window.innerWidth)
+  let containers = document.querySelectorAll('.container')
+  let marginLeft = (rect.width-320)/2-4+'px'
+  console.log(marginLeft)
+  containers.forEach(containerr => {
+    containerr.style.marginLeft = marginLeft
+  });
+
+  let menu = document.querySelector('.menu')
+  document.body.style.width = maxWidth
+  menu.style.width = `${maxWidth}`
 }
+phone_check()
 window.addEventListener('resize', phone_check)
