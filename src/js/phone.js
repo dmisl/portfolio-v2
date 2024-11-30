406ad38f6060a68490a6a212f96df9f3fb9dc178
unknown <rithlesscorp@gmail.com>
2024-11-30 16:08:35 +0100

    check

--- src/js/phone.js
+++ src/js/phone.js
@@ -1,11 +1,16 @@
-const element = document.querySelector('body');
-
-const rect = element.getBoundingClientRect();
-
-let maxWidth = rect.width+'px'
-
-let hero = document.querySelector('.hero')
-hero.style.width = maxWidth
-hero.style.overflow = 'hidden'
-
-console.log('Width:', rect.width);
+if(window.innerWidth <= 500)
+{
+  window.addEventListener('load', function () {
+    const element = document.querySelector('body');
+    
+    const rect = element.getBoundingClientRect();
+    
+    let maxWidth = rect.width+'px'
+    
+    let hero = document.querySelector('.hero')
+    hero.style.width = maxWidth
+    hero.style.overflow = 'hidden'
+    
+    console.log('Width:', rect.width);
+  })
+}
