bae6054525365f22ad809b17ee02ae396087cdf9
unknown <rithlesscorp@gmail.com>
2024-12-01 05:07:49 +0100

    function update

--- src/js/small-laptop.js
+++ src/js/small-laptop.js
@@ -1,7 +1,5 @@
 function check()
 {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
     if(window.innerWidth <= 1250 && window.innerWidth > 500)
     {
         document.querySelector('.about-me .text').innerHTML = `Hello, i’m <span>dmisl</span>!<br><br>I’m a self-taught full-stack developer based in <br><span>Warsaw, Poland</span>. I can develop responsive websites<br>from scratch and raise them into modern user-friendly<br>web experiences.<br><br>Transforming my creativity and knowledge into a websites has been my passion for years. I have been helping various clients to establish <br>their presence online. I always strive to learn about the newest technologies and frameworks.`
@@ -10,4 +8,8 @@ function check()
         document.querySelector('.about-me .text').innerHTML = `Hello, i’m <span>dmisl</span>!<br><br>I’m a self-taught full-stack developer based in <span>Warsaw, Poland</span>. I can develop responsive websites from<br>scratch and raise them into modern user-friendly web experiences.<br><br>Transforming my creativity and knowledge into a websites has been my passion for years. I have been helping various clients to establish <br>their presence online. I always strive to learn about the newest technologies and frameworks.`
     }
 }
-window.addEventListener('resize', check);
+let small_laptopTimeout
+window.addEventListener('resize', function () {
+    clearTimeout(small_laptopTimeout)
+    small_laptopTimeout = setTimeout(check, 500);
+});
